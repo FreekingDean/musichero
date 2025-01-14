e3230a45acc57ddd242319bdb41a5809a98531b3
Dean Galvin <deangalvin3@gmail.com>
2025-01-13 23:02:22 -0500

    Enforce sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::Base
+  include Pagy::Backend
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  include Pagy::Backend
+
+  before_action :authenticate_user!
+  helper_method :current_user
+
+  def authenticate_user!
+    return unless current_user.nil?
+
+    redirect_to new_user_session_path
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
